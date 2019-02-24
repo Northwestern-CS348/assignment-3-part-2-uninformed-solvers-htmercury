a830e3839396eae2646eb67d1a4be36e18db5880
Ka Wong <kawong2020@u.northwestern.edu>
2019-02-24 01:57:19 -0600

    flat files and game masters

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -34,7 +34,32 @@ class TowerOfHanoiGame(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### student code goes here
-        pass
+        peg1 = ()
+        peg2 = ()
+        peg3 = ()
+        # ask for all pegs
+        get_pegs = parse_input('fact: (inst ?p Peg)')
+        pegs = self.kb.kb_ask(get_pegs)
+        for peg in pegs:
+            is_this_peg_empty = Fact(instantiate(Statement(('empty', '?p')), peg)) # construct query fact
+            # check if peg is not empty
+            if is_this_peg_empty not in self.kb.facts:
+                # get the disks on this peg
+                get_disks = Fact(instantiate(Statement(('on', '?d', '?p')), peg))
+                disks = self.kb.kb_ask(get_disks)
+                # convert the list of disks to a list of integers representing the disks
+                disks = list(map(lambda d: int(d.bindings_dict['?d'].replace('disk', '')), disks))
+                disks.sort() # sort by acending order
+                # retrieve peg identity
+                peg_num = int(peg.bindings_dict['?p'].replace('peg', ''))
+                # switch cases
+                if peg_num is 1:
+                    peg1 = tuple(disks)
+                elif peg_num is 2:
+                    peg2 = tuple(disks)
+                else:
+                    peg3 = tuple(disks)
+        return (peg1, peg2, peg3)
 
     def makeMove(self, movable_statement):
         """
@@ -53,7 +78,41 @@ class TowerOfHanoiGame(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+        if (movable_statement.predicate != 'movable'):
+            return
+        (disk, src, dst) = movable_statement.terms
+        retract_facts = []
+        assert_facts = []
+        dst_top = None
+        src_below = None
+        # facts to be retracted
+        retract_facts.append(Fact(Statement(('on', disk, src))))
+        retract_facts.append(Fact(Statement(('top', disk, src))))
+        is_dst_empty = self.kb.kb_ask(Fact(Statement(('empty', dst))))
+        if is_dst_empty:
+            retract_facts.append(Fact(Statement(('empty', dst))))
+        else:
+            dst_top = self.kb.kb_ask(Fact(Statement(('top', '?d', dst))))[0]
+            retract_facts.append(Fact(instantiate(Statement(('top', '?d', dst)), dst_top)))
+        is_src_least_two = self.kb.kb_ask(Fact(Statement(('onTop', disk, '?d'))))
+        if is_src_least_two:
+            src_below = is_src_least_two[0]
+            retract_facts.append(Fact(instantiate(Statement(('onTop', disk, '?d')), src_below)))
+        # facts to be added
+        assert_facts.append(Fact(Statement(('on', disk, dst))))
+        assert_facts.append(Fact(Statement(('top', disk, dst))))
+        if not is_dst_empty:
+            assert_facts.append(Fact(instantiate(Statement(('onTop', disk, '?d')), dst_top)))
+        if is_src_least_two:
+            assert_facts.append(Fact(instantiate(Statement(('top', '?d', src)), src_below)))
+        else:
+            assert_facts.append(Fact(Statement(('empty', src))))
+
+        # retract/add facts
+        for r in retract_facts:
+            self.kb.kb_retract(r)
+        for a in assert_facts:
+            self.kb.kb_assert(a)      
 
     def reverseMove(self, movable_statement):
         """
@@ -100,7 +159,18 @@ class Puzzle8Game(GameMaster):
             A Tuple of Tuples that represent the game state
         """
         ### Student code goes here
-        pass
+        state = [[],[],[]]
+        for y in range(3):
+            for x in range(3):
+                what_tile_is_here = Fact(Statement(('position', '?t', f'pos{x+1}', f'pos{y+1}')))
+                tile = self.kb.kb_ask(what_tile_is_here)[0]
+                if (tile.bindings_dict['?t'] == 'empty'):
+                    state[y].append(-1)
+                else:
+                    tile_num = int(tile.bindings_dict['?t'].replace('tile', ''))
+                    state[y].append(tile_num)
+        
+        return tuple(map(lambda r: tuple(r), state))
 
     def makeMove(self, movable_statement):
         """
@@ -119,7 +189,20 @@ class Puzzle8Game(GameMaster):
             None
         """
         ### Student code goes here
-        pass
+        if (movable_statement.predicate != 'movable'):
+            return
+        (tile, src_x, src_y, dst_x, dst_y) = movable_statement.terms
+        # facts to be retracted
+        old_fact1 = Fact(Statement(('position', tile, src_x, src_y)))
+        old_fact2 = Fact(Statement(('position', 'empty', dst_x, dst_y)))
+        # facts to be added
+        new_fact1 = Fact(Statement(('position', 'empty', src_x, src_y)))
+        new_fact2 = Fact(Statement(('position', tile, dst_x, dst_y)))
+        # retract/add facts
+        self.kb.kb_retract(old_fact1)
+        self.kb.kb_retract(old_fact2)
+        self.kb.kb_assert(new_fact1)
+        self.kb.kb_assert(new_fact2)
 
     def reverseMove(self, movable_statement):
         """
